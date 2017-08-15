a89ec6b703770cf6cb68161b269dc0f6d4617055
Chen, Lei <LChen@ETS.ORG>
2017-08-15 14:29:04 -0400

    major update.
    - previously, wrongly used .ref and .hyp
    - now, for ref, can deal with optional top line showing file id
    - added file id as the first token, when using wer, need —head-ids
    option

--- wer.py
+++ wer.py
@@ -1,36 +1,44 @@
 import io
 import string
 import os
+import re
 
-def one_ref(fileName):
-    one_ln = ''
-    with io.open(fileName, 'r', encoding='utf-8') as f:
-        for ln in f:
-            for c in ['%HESITATION']:
-                ln = ln.replace(c, " ")
-            one_ln += ln.lower().rstrip('\n')
-    return one_ln
-
-def one_hyp(fileName):
-    one_ln = ''
+def one_ref(fileName, id):
+    one_ln = id
     with io.open(fileName, 'r', encoding='windows-1252') as f:
         for ln in f:
-            for c in string.punctuation:
+           ln = ln.lower().rstrip('\n')
+           if re.search(r't\d+_v\d+', ln):
+                pass
+           else:
+               for c in string.punctuation:
+                   ln = ln.replace(c, " ")
+               one_ln += " " + ln
+    return one_ln
+
+
+def one_hyp(fileName, id):
+    one_ln = id + ' '
+    with io.open(fileName, 'r', encoding='utf-8') as f:
+        for ln in f:
+            for c in ['%HESITATION', '*']:
                 ln = ln.replace(c, " ")
             one_ln += ln.lower().rstrip('\n')
     return one_ln
 
 
-def gen_set(hyp_dir, ref_dir):
+def gen_set(ref_dir, hyp_dir):
     out_hyp = io.open('one.hyp', 'w', encoding='utf-8')
     out_ref = io.open('one.ref', 'w', encoding='utf-8')
-    for hyp in os.listdir(hyp_dir):
-        if hyp.endswith('.txt'):
-            print(hyp)
 
-            ref = hyp.replace('txt', 'md')
-            hyp_ln = one_hyp(hyp_dir + '/' + hyp)
-            ref_ln = one_ref(ref_dir + '/SELTCAWRS_' + ref)
+    for ref in os.listdir(ref_dir):
+        if ref.endswith('.txt'):
+            print(ref)
+            id = os.path.basename(ref)
+
+            hyp = ref.replace('txt', 'md')
+            ref_ln = one_ref(ref_dir + '/' + ref, id)
+            hyp_ln = one_hyp(hyp_dir + '/SELTCAWRS_' + hyp, id)
             out_hyp.write(hyp_ln + '\n')
             out_ref.write(ref_ln + '\n')
     out_hyp.close()
