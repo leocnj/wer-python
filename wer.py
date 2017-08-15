0efb07e73621258de81f944b7e6166883e1364fa
Chen, Lei <LChen@ETS.ORG>
2017-08-15 12:28:34 -0400

    can work on real data now.
    - TODO: deal with the top lines using file ids in some ref files

--- wer.py
+++ wer.py
@@ -25,13 +25,16 @@ def gen_set(hyp_dir, ref_dir):
     out_hyp = io.open('one.hyp', 'w', encoding='utf-8')
     out_ref = io.open('one.ref', 'w', encoding='utf-8')
     for hyp in os.listdir(hyp_dir):
-        if hyp.endswith('.hyp'):
-            ref = hyp.replace('hyp', 'ref')
+        if hyp.endswith('.txt'):
+            print(hyp)
+
+            ref = hyp.replace('txt', 'md')
             hyp_ln = one_hyp(hyp_dir + '/' + hyp)
-            ref_ln = one_ref(ref_dir + '/' + ref)
-            out_hyp.write(hyp_ln)
-            out_ref.write(ref_ln)
+            ref_ln = one_ref(ref_dir + '/SELTCAWRS_' + ref)
+            out_hyp.write(hyp_ln + '\n')
+            out_ref.write(ref_ln + '\n')
     out_hyp.close()
     out_ref.close()
 
-gen_set('trans', 'trans')
+# gen_set('trans', 'trans')
+gen_set('trans/txt', 'trans/md')
